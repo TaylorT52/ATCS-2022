379b8ec2ca813440d979adcf025d5961511f0cd0
Taylor Tam <taylor@taylortam.com>
2022-11-09 07:08:28 -0800

    added slide animation

--- portfolio/scripts/about.js
+++ portfolio/scripts/about.js
@@ -1,23 +1,47 @@
 const interval = 200; 
-const minPos = -500; 
+const intervalSlide = 100;
+const minPosEx = -3500; 
 var lastScroll = 0; 
-var triggered = false;
+var triggeredEx = false;
+var triggeredLs = false;
+var minPosLs = -500; 
+
+var slideElements = document.querySelectorAll('.fade-left')
 var elementsToShow = document.querySelectorAll('.show-on-scroll');
+elementsToSlide = Array.from(slideElements); 
 elementsToShow = Array.from(elementsToShow); 
 var scroll = window.requestAnimationFrame || function(callback){ window.setTimeout(callback, 1000/60)};
 
 window.addEventListener('scroll', () => {
     var test = document.documentElement.getBoundingClientRect().top; 
-    if(test < minPos){
-        if(!triggered){
+    if(test < minPosEx){
+        if(!triggeredEx){
             show(elementsToShow); 
+        }
+    }else if(test < minPosLs){
+        if(!triggeredLs){
+            console.log(elementsToSlide.length);
+            slideIn(elementsToSlide); 
         }
     }
     lastScroll = test; 
 })
 
+function slideIn(arr) {
+    if (arr.length == 0) {
+        triggeredLs = true;
+        return;
+    }
+    arr[0].classList.add("active");
+    arr[0].classList.remove("not-visible")
+
+    setTimeout(() => {
+        slideIn(arr.slice(1))
+    }, intervalSlide);
+}
+
 function show(arr) {
-    if (arr.length === 0) {
+    if (arr.length == 0) {
         return;
     }
     arr[0].style.opacity = 1; 
